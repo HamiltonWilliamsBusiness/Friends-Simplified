d68a8c67ef53cddfe8c5e97308bdb0baca44833d
Hamilton Williams <hamiltonwilliamsbusiness@gmail.com>
2023-11-20 14:25:00 -0600

    Feat: fixed errors within getFriend controller function

--- backend/controllers/friendController.js
+++ backend/controllers/friendController.js
@@ -1,8 +1,32 @@
 const Friend = require('../models/friendModel')
+const mongoose = require('mongoose')
 
 // get all friends
+const getFriends = async (req, res) => {
+    const friends = await Friend.find({}).sort({createdAt: -1})
+
+    res.status(200).json(friends)
+}
 
 // get a single friend
+const getFriend = async (req, res) => {
+    const { id } = req.params
+
+    // Verify ObjectId is a mongoDB objectID
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: 'No such workout'})
+    }
+
+    // Find Friend
+    const friend = await Friend.findById(id)
+
+    // If friend is null, then respond with an error
+    if (!friend){
+        return res.status(404).json({error: 'No such workout'})
+    }
+
+    res.status(200).json(friend)
+}
 
 // create a new friend
 const createFriend = async (req, res) => {
@@ -22,5 +46,7 @@ const createFriend = async (req, res) => {
 // update a friend
 
 module.exports = {
+    getFriend,
+    getFriends,
     createFriend
 }
--- backend/routes/friends.js
+++ backend/routes/friends.js
@@ -1,28 +1,26 @@
 const express = require('express')
 const {
     createFriend,
+    getFriend,
+    getFriends,
 } = require('../controllers/friendController')
 const router = express.Router()
 
 // Get all friends
-router.get('/', (req, res) => {
-    res.json({mssg: 'GET all friends'})
-})
+router.get('/', getFriends)
 
 // Get a single friend
-router.get('/:id', (req, res) => {
-    res.json({mssg: 'GET a single friend'})
-})
+router.get('/:id', getFriend)
 
 // POST a new friend
 router.post('/', createFriend)
 
-// Delete a new friend
+// DELETE a new friend
 router.delete('/:id', (req, res) => {
     res.json({mssg: 'DELETE a new friend'})
 })
 
-// POST a new friend
+// UPDATE a new friend
 router.patch('/:id', (req, res) => {
     res.json({mssg: 'Update a new friend'})
 })
