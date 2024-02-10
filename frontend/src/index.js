03acf3c663ef8821463ad2e7f09435c1f640d8fe
Hamilton Williams <hamiltonwilliamsbusiness@gmail.com>
2024-02-10 10:24:02 -0600

    Feat: created authContext & custom hook to track user state

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { FriendsContextProvider } from './context/FriendContext';
+import { AuthContextProvider } from './context/AuthContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <FriendsContextProvider>
-      <App />
-    </FriendsContextProvider>
+    <AuthContextProvider>
+      <FriendsContextProvider>
+        <App />
+      </FriendsContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
 
