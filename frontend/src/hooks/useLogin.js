924482a888893c9478d55454452a2a0d5d025823
Hamilton Williams <hamiltonwilliamsbusiness@gmail.com>
2024-03-25 14:34:08 -0500

    Feat: created useLogin hook

---
+++ frontend/src/hooks/useLogin.js
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import { useAuthContext } from './useAuthContext'
+
+export const useLogin = () => {
+    const [error, setError] = useState(null)
+    const [isLoading, setIsLoading] = useState(null)
+    const { dispatch } = useAuthContext()
+
+    const login = async (email, password) => {
+        setIsLoading(true)
+        setError(null)
+
+        const response = await fetch('/api/user/login', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({email, password})
+        })
+        const json = await response.json()
+
+        // if not a success we will recieve an error message
+        // status(400), ok will be false
+        // this checks if ok is false
+        if (!response.ok) {
+            setIsLoading(false)
+            // we are sent an error property back when
+            // there is an error
+            setError(json.error)
+        }
+        if (response.ok) {
+
+            // save the user to local storage
+            // This stores token and email
+            localStorage.setItem('user', JSON.stringify(json))
+
+            dispatch({type: 'LOGIN', payload: json})
+
+            setIsLoading(false)
+        }
+    }
+
+    return { login, isLoading, error }
+}
